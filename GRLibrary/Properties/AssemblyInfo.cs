ebd4caf6cba3760d527427b27e3e1450f67aed52
anionDev <marius.goecke@gmail.com>
2018-05-06 12:43:13 +0200

    refactoring

--- GRLibrary/Properties/AssemblyInfo.cs
+++ GRLibrary/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GRLibrary")]
 [assembly: AssemblyDescription("")]
@@ -8,7 +9,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ce222290-9685-4147-a0c5-674df152c2ed")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion(Version.AssemblyVersion)]
+[assembly: AssemblyFileVersion(Version.AssemblyFileVersion)]
+[assembly: AssemblyInformationalVersion(Version.AssemblyInformationalVersion)]
+internal static class Version
+{
+    public const string AssemblyVersion = "0.1.0.0";
+    public const string AssemblyFileVersion = AssemblyVersion;
+    public const string AssemblyInformationalVersion = AssemblyVersion;
+}
