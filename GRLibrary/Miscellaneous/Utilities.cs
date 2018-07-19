2c0cb2c3b09cc893eab1a6e65c6ea497f4b120e1
anionDev <marius.goecke@gmail.com>
2018-07-19 08:14:04 +0200

    refactoring

--- GRLibrary/Miscellaneous/Utilities.cs
+++ GRLibrary/Miscellaneous/Utilities.cs
@@ -184,18 +184,27 @@ namespace GRLibrary
         //see https://stackoverflow.com/a/17190236/3905529
         public static bool IsList(this object o)
         {
-            if (o == null) return false;
-            return o is IList &&
-                   o.GetType().IsGenericType &&
-                   o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+            if (o == null) { return false; }
+            else
+            {
+                return o is IList &&
+                       o.GetType().IsGenericType &&
+                       o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+            }
         }
 
         public static bool IsDictionary(this object o)
         {
-            if (o == null) return false;
-            return o is IDictionary &&
+            if (o == null)
+            {
+                return false;
+            }
+            else
+            {
+                return o is IDictionary &&
                    o.GetType().IsGenericType &&
                    o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(Dictionary<,>));
+            }
         }
         #endregion
         public static bool IsSet(this object o)
