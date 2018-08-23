925d89f97ea3112de4f70d73a4032cba79de5290
Marius Göcke <marius.goecke@gmail.com>
2018-08-23 17:13:59 +0200

    added .m3uconfiguration-option with replace-function

--- GRLibrary/Miscellaneous/Playlists/ConcretePlaylistHandler/M3UHandler.cs
+++ GRLibrary/Miscellaneous/Playlists/ConcretePlaylistHandler/M3UHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
@@ -40,12 +41,64 @@ namespace GRLibrary.Miscellaneous.Playlists.ConcretePlaylistHandler
                     result.Add(item);
                 }
             }
-            string m3uConfigurationFile = new FileInfo(playlistFile).Directory.FullName+"\\.M3UConfiguration";
+            string m3uConfigurationFile = new FileInfo(playlistFile).Directory.FullName + "\\.M3UConfiguration";
             if (File.Exists(m3uConfigurationFile))
             {
-                throw new System.NotImplementedException();
+                M3UConfiguration configuration = new M3UConfiguration(m3uConfigurationFile);
+                result = configuration.ApplyTo(result).ToList();
             }
             return result;
+        }
+        private class M3UConfiguration
+        {
+            private readonly Dictionary<string, string> _Replace = new Dictionary<string, string>();
+            private readonly string _ConfigurationFile;
+            public M3UConfiguration(string configurationFile)
+            {
+                this._ConfigurationFile = configurationFile;
+                ReadConfigFile();
+            }
+
+            private void ReadConfigFile()
+            {
+                foreach (string line in File.ReadAllLines(this._ConfigurationFile))
+                {
+                    try
+                    {
+                        if (line.Contains(":"))
+                        {
+                            string optionKey = line.Split(':')[0].ToLower();
+                            string optionValue = line.Substring(optionKey.Length + 1);
+                            if (optionKey.Equals("replace"))
+                            {
+                                string[] splitted = optionValue.Split(';');
+                                this._Replace.Add(splitted[0], splitted[1]);
+                            }
+                            //add other options if desired
+                        }
+                    }
+                    catch
+                    {
+                        Utilities.NoOperation();
+                    }
+                }
+            }
+
+            internal IEnumerable<string> ApplyTo(IEnumerable<string> input)
+            {
+                List<string> result = new List<string>();
+                foreach (string item in input)
+                {
+                    string newItem = item;
+                    foreach (KeyValuePair<string, string> replacement in this._Replace)
+                    {
+                        newItem = newItem.Replace(replacement.Key, replacement.Value);
+                    }
+                    //process other options if available
+                    result.Add(newItem);
+                }
+                return result;
+            }
         }
     }
 }
