ac89ecb567ae65fffd314f90c8b7f5171813a1fa
Marius Göcke <marius.goecke@gmail.com>
2018-12-09 13:21:36 +0100

    implemented trivial stuff for StreamMixer

--- GRYLibrary/Miscellaneous/StreamMixer.cs
+++ GRYLibrary/Miscellaneous/StreamMixer.cs
@@ -7,31 +7,32 @@ namespace GRYLibrary.Miscellaneous
     {
         private IDictionary<string, Stream> Outputs { get; set; } = new Dictionary<string, Stream>();
         private IDictionary<string, Stream> Inputs { get; set; } = new Dictionary<string, Stream>();
-        private IList<TupleWithValueComparisonEquals<string, string>> Connections { get; set; } = new List<TupleWithValueComparisonEquals<string, string>>();
+        private ISet<TupleWithValueComparisonEquals<string, string>> Connections { get; set; } = new HashSet<TupleWithValueComparisonEquals<string, string>>();
         public void AddInputStream(string name, Stream stream)
         {
-            throw new System.NotImplementedException();
+            this.Inputs.Add(name, stream);
         }
         public void RemoveInputStream(string name)
         {
-            throw new System.NotImplementedException();
+            this.Inputs.Remove(name);
         }
         public void AddOutputStream(string name, Stream stream)
         {
-            throw new System.NotImplementedException();
+            this.Outputs.Add(name, stream);
         }
         public void RemoveOutputStream(string name)
         {
-            throw new System.NotImplementedException();
+            this.Outputs.Remove(name);
         }
         public void AddConnection(string inputName, string outputName)
         {
+            Connections.Add(new TupleWithValueComparisonEquals<string, string>(inputName, outputName));
             throw new System.NotImplementedException();
         }
         public void RemoveConnection(string inputName, string outputName)
         {
+            Connections.Remove(new TupleWithValueComparisonEquals<string, string>(inputName, outputName));
             throw new System.NotImplementedException();
         }
     }
 }
-
