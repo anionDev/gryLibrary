238be245c0feb89eca51d658c1eea775e93f29ce
Marius Göcke <marius.goecke@gmail.com>
2018-12-19 19:54:08 +0100

    fixed CommonTestM3UConfigurationWithAbsolutePath-test

--- GRYLibraryTest/PlaylistTest.cs
+++ GRYLibraryTest/PlaylistTest.cs
@@ -195,7 +195,7 @@ namespace GRYLibraryTest
             M3UHandler.Instance.AddSongsToPlaylist(m3uFile2, new string[] { "trackC.mp3", "{DefaultPath}\\trackD.mp3" });
 
             HashSet<string> playlistItems = new HashSet<string>(M3UHandler.Instance.GetSongsFromPlaylist(m3uFile1, true, true));
-            Assert.IsTrue(playlistItems.SetEquals(new string[] { "trackA.mp3", @"C:\Data\Music\trackB.mp3", "trackC.mp3", @"C:\Data\Music\trackD.mp3" }));
+            Assert.IsTrue(playlistItems.SetEquals(new string[] { System.IO.Path.Combine(directoryName, "trackA.mp3"), @"C:\Data\Music\trackB.mp3", System.IO.Path.Combine(directoryName, "trackC.mp3"), @"C:\Data\Music\trackD.mp3" }));
             Utilities.EnsureFileDoesNotExist(m3uFile1);
             Utilities.EnsureFileDoesNotExist(m3uFile2);
             Utilities.EnsureDirectoryDoesNotExist(directoryName);
