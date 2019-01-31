095fd36290d3d8a8125b486840b575f6f810f2bd
Marius Göcke <marius.goecke@gmail.com>
2019-01-31 07:37:43 +0100

    added TaskQueue-class

---
+++ GRYLibrary/Miscellaneous/TaskQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GRYLibrary.Miscellaneous
+{
+    public class TaskQueue
+    {
+        public void Queue(Action action)
+        {
+            throw new NotImplementedException();
+        }
+        public int MaxDegreeOfParallelism { get; set; }
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
