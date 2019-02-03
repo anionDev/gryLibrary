5f5e17e3836a61dde22c4cc358169247dcb81c78
Marius Göcke <marius.goecke@gmail.com>
2019-02-03 19:54:30 +0100

    added log for TaskQueue

--- GRYLibrary/Miscellaneous/TaskQueue.cs
+++ GRYLibrary/Miscellaneous/TaskQueue.cs
@@ -7,15 +7,17 @@ namespace GRYLibrary.Miscellaneous
 {
     public class TaskQueue
     {
+        private readonly Queue<Tuple<string, Action>> _ActionQueue = new Queue<Tuple<string, Action>>();
         public TaskQueue(bool infiniteMode = false)
         {
-            this.CurrentAmountOfThreads =new Semaphore(nameof(this.CurrentAmountOfThreads));
+            this.CurrentAmountOfThreads = new Semaphore(nameof(this.CurrentAmountOfThreads));
             this.IsRunning = false;
             this.InfiniteMode = infiniteMode;
             this.MaxDegreeOfParallelism = 10;
         }
-        private readonly Queue<Action> _ActionQueue = new Queue<Action>();
-        public void Queue(Action action)
+
+        /// <remarks>The string-value is supposed to be the name of the action.</remarks>
+        public void Queue(Tuple<string, Action> action)
         {
             this._ActionQueue.Enqueue(action);
         }
@@ -23,6 +25,7 @@ namespace GRYLibrary.Miscellaneous
         public bool IsRunning { get; private set; }
         public bool InfiniteMode { get; }
         public int MaxDegreeOfParallelism { get; set; }
+        public GRYLog LogObject { get; set; } = null;
         public void Start()
         {
             if (!this.IsRunning)
@@ -35,6 +38,7 @@ namespace GRYLibrary.Miscellaneous
         {
             try
             {
+                LogObject?.LogInformation($"Start executing queued actions.");
                 bool enabled = true;
                 while (enabled)
                 {
@@ -54,8 +58,8 @@ namespace GRYLibrary.Miscellaneous
             finally
             {
                 this.IsRunning = false;
+                LogObject?.LogInformation($"Finished executing queued actions.");
             }
-
         }
 
         private bool IsFinished()
@@ -68,20 +72,22 @@ namespace GRYLibrary.Miscellaneous
             return 0 < this._ActionQueue.Count && this.CurrentAmountOfThreads.Value < this.MaxDegreeOfParallelism;
         }
 
-        private void ExecuteTask(Action action)
+        private void ExecuteTask(Tuple<string, Action> action)
         {
             this.CurrentAmountOfThreads.Increment();
+            LogObject?.LogInformation($"Start action {action.Item1}.");
             try
             {
-                Task.Run(action).Wait();
+                Task.Run(action.Item2).Wait();
             }
-            catch
+            catch (Exception exception)
             {
-                Utilities.NoOperation();
+                LogObject?.LogError($"Error in action {action.Item1}.", exception);
             }
             finally
             {
                 this.CurrentAmountOfThreads.Decrement();
+                LogObject?.LogInformation($"Finished action {action.Item1}.");
             }
         }
     }
