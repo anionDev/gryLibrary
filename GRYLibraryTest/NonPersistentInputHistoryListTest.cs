b1a9ff36d72fa206c7f8e92e0e45872c3a6456ad
Marius Göcke <marius.goecke@gmail.com>
2019-04-13 23:02:42 +0200

    added some tests

---
+++ GRYLibraryTest/NonPersistentInputHistoryListTest.cs
@@ -0,0 +1,76 @@
+﻿
+using GRYLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GRYLibraryTest
+{
+    [TestClass]
+    class NonPersistentInputHistoryListTest
+    {
+        [TestMethod]
+        public void NonPersistentInputHistoryListTest1()
+        {
+            var inpustList = new NonPersistentInputHistoryList();
+
+            string input1 = "1";
+            string input2 = "2";
+            string input3 = "3";
+            string input4 = "4";
+            string input5 = "5";
+
+            Assert.AreEqual(string.Empty, inpustList.DownPressed());
+            Assert.AreEqual(string.Empty, inpustList.UpPressed());
+
+            inpustList.EnterPressed(input1);
+
+            Assert.AreEqual(input2, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.UpPressed());
+
+            inpustList.EnterPressed(input2);
+
+            Assert.AreEqual(input2, inpustList.UpPressed());
+            Assert.AreEqual(input1, inpustList.UpPressed());
+            Assert.AreEqual(input1, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.DownPressed());
+
+            inpustList.EnterPressed(input3);
+            inpustList.EnterPressed(input4);
+
+            Assert.AreEqual(input4, inpustList.UpPressed());
+            Assert.AreEqual(input3, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.UpPressed());
+            Assert.AreEqual(input1, inpustList.UpPressed());
+            Assert.AreEqual(input1, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.DownPressed());
+            Assert.AreEqual(input3, inpustList.DownPressed());
+            Assert.AreEqual(input4, inpustList.DownPressed());
+            Assert.AreEqual(string.Empty, inpustList.DownPressed());
+            Assert.AreEqual(string.Empty, inpustList.DownPressed());
+            
+            Assert.AreEqual(input4, inpustList.UpPressed());
+            Assert.AreEqual(input3, inpustList.UpPressed());
+
+            inpustList.EnterPressed(input5);
+            
+            Assert.AreEqual(input5, inpustList.UpPressed());
+            Assert.AreEqual(input4, inpustList.UpPressed());
+            Assert.AreEqual(input3, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.UpPressed());
+            Assert.AreEqual(input1, inpustList.UpPressed());
+            Assert.AreEqual(input1, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.DownPressed());
+            Assert.AreEqual(input3, inpustList.DownPressed());
+            Assert.AreEqual(input4, inpustList.DownPressed());
+            Assert.AreEqual(input5, inpustList.DownPressed());
+            Assert.AreEqual(string.Empty, inpustList.DownPressed());
+            Assert.AreEqual(input5, inpustList.UpPressed());
+            Assert.AreEqual(input4, inpustList.UpPressed());
+            Assert.AreEqual(input3, inpustList.UpPressed());
+            Assert.AreEqual(input2, inpustList.UpPressed());
+            Assert.AreEqual(input3, inpustList.DownPressed());
+            Assert.AreEqual(input2, inpustList.UpPressed());
+            Assert.AreEqual(input3, inpustList.DownPressed());
+
+        }
+    }
+}
