8fb7004b36e350347d5aba2587e2ec54306d4e01
Marius Göcke <marius.goecke@gmail.com>
2019-04-13 23:55:38 +0200

    added test

--- GRYLibraryTest/COlorGradientTest.cs
+++ GRYLibraryTest/COlorGradientTest.cs
@@ -4,13 +4,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GRYLibraryTest
 {
     [TestClass]
-   public class ColorGradientTest
+    public class ColorGradientTest
     {
         [TestMethod]
         public void ColorGradientTest1()
         {
-            var startColor = new ExtendedColor(255,142,0);
-            var endColor = new ExtendedColor(100,100,100);
+            var startColor = new ExtendedColor(255, 142, 0);
+            var endColor = new ExtendedColor(100, 100, 100);
             var colorGradient = new ColorGradient(startColor, endColor);
             Assert.AreEqual(new ExtendedColor(100, 100, 100), colorGradient.GetColorGradientValue(0));
             Assert.AreEqual(null, colorGradient.GetColorGradientValue(1));
