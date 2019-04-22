8d22c5a3e6604ade1db7e9934d09b29dc241a028
Marius Göcke <marius.goecke@gmail.com>
2019-04-23 01:33:44 +0200

    fixed testcases

--- GRYLibraryTest/ExtendedColorTest.cs
+++ GRYLibraryTest/ExtendedColorTest.cs
@@ -21,7 +21,6 @@ namespace GRYLibraryTest
             Assert.AreEqual(color1, color2);
             Assert.AreEqual(color1.GetHashCode(), color2.GetHashCode());
         }
-        [Ignore]
         [TestMethod]
         public void ExtendedColorTest3()
         {
@@ -31,16 +30,16 @@ namespace GRYLibraryTest
             byte testValueB = 10;
 
             var color = new ExtendedColor(testValueA,testValueR,testValueG,testValueB);
-            Assert.AreEqual(0, color.ColorCode);
+            Assert.AreEqual(-922812406, color.ColorCode);
             Assert.AreEqual(testValueA, color.A);
             Assert.AreEqual(testValueR, color.R);
             Assert.AreEqual(testValueG, color.G);
             Assert.AreEqual(testValueB, color.B);
             Assert.AreEqual(System.Drawing.Color.FromArgb(testValueA,testValueR,testValueG,testValueB), color.DrawingColor);
-            Assert.AreEqual("", color.GetARGBString());
-            Assert.AreEqual("", color.GetRGBString());
+            Assert.AreEqual("C8FF000A", color.GetARGBString());
+            Assert.AreEqual("FF000A", color.GetRGBString());
             Assert.AreEqual(Color.FromArgb(testValueA, testValueR, testValueG, testValueB), color.MediaColor);
-            Assert.AreEqual("", color.ToString());
+            Assert.AreEqual("ExtendedColor(A=200,R=255,G=0,B=10)", color.ToString());
         }
     }
 }
