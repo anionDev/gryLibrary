ac58bb0f246eb1002a49e7432e5c0d7f18614323
Marius Göcke <marius.goecke@gmail.com>
2019-05-21 17:29:27 +0200

    added testexecution in buildscript

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -10,3 +10,8 @@ nuget restore -SolutionDirectory ..
 msbuild GRYLibraryTest.csproj /t:Build /verbosity:normal /property:Configuration=Release
 popd
 
+push ..\GRYLibraryTest\bin\Release
+vstest.console GRYLibraryTest.dll
+set vstestResult = %errorlevel%
+popd
+exit /b %vstestResult%
