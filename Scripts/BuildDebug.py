520a5ef7c645f0f0f4a8dc315f4fc22dc903d2e1
Marius Göcke <marius.goecke@gmail.com>
2019-06-18 11:47:42 +0200

    updated buildscripts

---
+++ Scripts/BuildDebug.py
@@ -0,0 +1,3 @@
+import subprocess
+print("Start GRYLibrary Build.py Debug")
+subprocess.call("python Build.py Debug")
---
+++ Scripts/BuildRelease.py
@@ -0,0 +1,3 @@
+import subprocess
+print("Start GRYLibrary Build.py Release")
+subprocess.call("python Build.py Release")
