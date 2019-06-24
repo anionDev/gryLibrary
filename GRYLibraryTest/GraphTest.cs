69a39bb294b82c6ce4bfad29bb59d92ff4dda2bc
Marius Göcke <marius.goecke@gmail.com>
2019-06-24 12:46:16 +0200

    added testcases

--- GRYLibraryTest/GraphTest.cs
+++ GRYLibraryTest/GraphTest.cs
@@ -81,7 +81,6 @@ namespace GRYLibraryTest
             Assert.AreEqual(5, g.Edges.Count());
             Assert.AreEqual(2, v1.ConnectedEdges.Count());
 
-
             //test TryGetConnectionBetween:
             Edge e1Reloaded;
             Assert.IsTrue(g.TryGetConnectionBetween(v1, v2, out e1Reloaded));
@@ -127,6 +126,93 @@ namespace GRYLibraryTest
             Assert.AreEqual(1, successorsOfv5.Count);
             Assert.AreEqual(v1, successorsOfv5.First());
 
+            Edge e43 = new Edge(v4, v3);
+            g.AddEdge(e43);
+            ISet<Vertex> successorsOfv4 = v4.GetSuccessors(g);
+            Assert.AreEqual(2, successorsOfv4.Count);
+            Assert.IsTrue(new HashSet<Vertex>() { v3, v5 }.SetEquals(successorsOfv4));
+        }
+        [TestMethod]
+        public void SimpleUndrectedGraphTest()
+        {
+            UndirectedGraph g = new UndirectedGraph();
+            Assert.IsTrue(g.SelfLoopIsAllowed);
+            Vertex v1 = new Vertex("v1");
+            Vertex v2 = new Vertex("v2");
+            Vertex v3 = new Vertex("v3");
+            Vertex v4 = new Vertex("v4");
+            Vertex v5 = new Vertex("v5");
+
+            Edge e1 = new Edge(v1, v2, "e1");
+            Edge e2 = new Edge(v2, v3, "e2");
+            Edge e3 = new Edge(v3, v4, "e3");
+            Edge e4 = new Edge(v4, v5, "e4");
+            Edge e5 = new Edge(v5, v1, "e5");
+
+            g.AddVertex(v1);
+            g.AddVertex(v2);
+            g.AddVertex(v3);
+            g.AddVertex(v4);
+            g.AddVertex(v5);
+            g.AddEdge(e1);
+            g.AddEdge(e2);
+            g.AddEdge(e3);
+            g.AddEdge(e4);
+            g.AddEdge(e5);
+
+            Assert.AreEqual(5, g.Vertices.Count());
+            Assert.AreEqual(5, g.Edges.Count());
+            Assert.AreEqual(2, v1.ConnectedEdges.Count());
+
+            //test TryGetConnectionBetween:
+            Edge e1Reloaded1;
+            Assert.IsTrue(g.TryGetConnectionBetween(v1, v2, out e1Reloaded1));
+            Assert.AreEqual(e1, e1Reloaded1);
+            Edge e1Reloaded2;
+            Assert.IsTrue(g.TryGetConnectionBetween(v2, v1, out e1Reloaded2));
+            Assert.AreEqual(e1, e1Reloaded2);
+
+            Assert.AreEqual(e1Reloaded1, e1Reloaded2);
+
+            Edge e42 = new Edge(v5, v4);
+            try
+            {
+                g.AddEdge(e42);//edge this this source and target does already exist
+                Assert.Fail();
+            }
+            catch
+            {
+            }
+
+            //test TryGetConnectionBetween with selfloop:
+            Edge eSelfLoop = new Edge(v1, v1);
+            g.AddEdge(eSelfLoop);
+            Edge eSelfLoopReloaded;
+            g.TryGetConnectionBetween(v1, v1, out eSelfLoopReloaded);
+            Assert.AreEqual(eSelfLoop, eSelfLoopReloaded);
+            Assert.AreEqual(5, g.Vertices.Count());
+            Assert.AreEqual(6, g.Edges.Count());
+
+            try
+            {
+                g.SelfLoopIsAllowed = false;//g does already have a selfloop
+                Assert.Fail();
+            }
+            catch
+            {
+            }
+            Assert.IsTrue(g.SelfLoopIsAllowed);
+
+            g.RemoveEdge(eSelfLoop);
+            Assert.IsFalse(g.TryGetConnectionBetween(v1, v1, out _));
+            g.SelfLoopIsAllowed = false;
+            Assert.IsFalse(g.SelfLoopIsAllowed);
+            Assert.AreEqual(5, g.Vertices.Count());
+            Assert.AreEqual(5, g.Edges.Count());
+
+            ISet<Vertex> successorsOfv4 = v4.GetSuccessors(g);
+            Assert.AreEqual(2, successorsOfv4.Count);
+            Assert.IsTrue(new HashSet<Vertex>() { v3, v5 }.SetEquals(successorsOfv4));
         }
 
     }
