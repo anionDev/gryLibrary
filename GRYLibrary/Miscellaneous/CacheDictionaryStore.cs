198aa34d1aabc2303322ad006bbf568f87582b89
Marius Göcke <marius.goecke@gmail.com>
2019-09-26 18:00:29 +0200

    fixed type

--- GRYLibrary/Miscellaneous/CacheDictionaryStore.cs
+++ GRYLibrary/Miscellaneous/CacheDictionaryStore.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 
 namespace GRYLibrary.Miscellaneous
 {
-    public class CachDictionaryStore<TKey, TValue>
+    public class CacheDictionaryStore<TKey, TValue>
     {
         private readonly IDictionary<TKey, TValue> _Cache = new Dictionary<TKey, TValue>();
         private readonly Func<TKey, TValue> _GetFunction = null;
 
-        public CachDictionaryStore(Func<TKey, TValue> getFunction)
+        public CacheDictionaryStore(Func<TKey, TValue> getFunction)
         {
             this._GetFunction = getFunction;
         }
 
         public TValue GetValue(TKey key)
         {
-            if (!_Cache.ContainsKey(key))
+            if (!this._Cache.ContainsKey(key))
             {
-                _Cache.Add(key, _GetFunction(key));
+                this._Cache.Add(key, this._GetFunction(key));
             }
-            return _Cache[key];
+            return this._Cache[key];
         }
         public void ResetCache()
         {
-            _Cache.Clear();
+            this._Cache.Clear();
         }
         public void ResetCache(TKey item)
         {
-            _Cache.Remove(item);
+            this._Cache.Remove(item);
         }
     }
 }
