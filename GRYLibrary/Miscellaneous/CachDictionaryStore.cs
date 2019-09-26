336b030716fcce3699fc0875500561b5d10812af
Marius Göcke <marius.goecke@gmail.com>
2019-09-26 17:57:23 +0200

    added cachedictionarystore

---
+++ GRYLibrary/Miscellaneous/CachDictionaryStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GRYLibrary.Miscellaneous
+{
+    public class CachDictionaryStore<TKey, TValue>
+    {
+        private readonly IDictionary<TKey, TValue> _Cache = new Dictionary<TKey, TValue>();
+        private readonly Func<TKey, TValue> _GetFunction = null;
+
+        public CachDictionaryStore(Func<TKey, TValue> getFunction)
+        {
+            this._GetFunction = getFunction;
+        }
+
+        public TValue GetValue(TKey key)
+        {
+            if (!_Cache.ContainsKey(key))
+            {
+                _Cache.Add(key, _GetFunction(key));
+            }
+            return _Cache[key];
+        }
+        public void ResetCache()
+        {
+            _Cache.Clear();
+        }
+        public void ResetCache(TKey item)
+        {
+            _Cache.Remove(item);
+        }
+    }
+}
