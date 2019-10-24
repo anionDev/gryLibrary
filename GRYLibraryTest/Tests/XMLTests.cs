918b7de4435f06230fb340c54666ea93d274e5bc
Marius Göcke <marius.goecke@gmail.com>
2019-10-24 14:45:19 +0200

    updated xslt

--- GRYLibraryTest/Tests/XMLTests.cs
+++ GRYLibraryTest/Tests/XMLTests.cs
@@ -7,25 +7,33 @@ namespace GRYLibraryTest.Tests
     [TestClass]
     public class XMLTests
     {
-        private static readonly Encoding _FileEncoding = new UTF8Encoding(false);
-        private static readonly string _TestDataFolder = Path.Combine("TestData", "XMLValidator", "Test1");
-        private static readonly string _TestXSD1File = Path.Combine(_TestDataFolder, "TestXSD1.xsd");
-        private static readonly string _TestXML1_MatchsXSD1 = Path.Combine(_TestDataFolder, "TestXML1_MatchsXSD1.xml");
-        private static readonly string _TestXML2_MatchsXSD1Not = Path.Combine(_TestDataFolder, "TestXML2_MatchsXSD1Not.xml");
-        private static readonly string _TestTargetNamespace = "mynamespace";
+        Encoding _FileEncoding = new UTF8Encoding(false);
         [TestMethod]
         public void TestXSDValidator_Test1()
         {
-            string xmlWorking = File.ReadAllText(_TestXML1_MatchsXSD1, _FileEncoding);
-            string xmlNotWorking = File.ReadAllText(_TestXML2_MatchsXSD1Not, _FileEncoding);
-            string xsd = File.ReadAllText(_TestXSD1File, _FileEncoding);
-            Assert.IsTrue(GRYLibrary.Utilities.ValidateXMLAgainstXSD(xmlWorking, xsd, _TestTargetNamespace, out _));
-            Assert.IsFalse(GRYLibrary.Utilities.ValidateXMLAgainstXSD(xmlNotWorking, xsd, _TestTargetNamespace, out _));
+            string testDataFolder = Path.Combine("TestData", "XMLValidator", "Test1");
+            string testXSD1File = Path.Combine(testDataFolder, "TestXSD1.xsd");
+            string testXML1_MatchsXSD1 = Path.Combine(testDataFolder, "TestXML1_MatchsXSD1.xml");
+            string testXML2_MatchsXSD1Not = Path.Combine(testDataFolder, "TestXML2_MatchsXSD1Not.xml");
+            string testTargetNamespace = "mynamespace";
+
+            string xmlWorking = File.ReadAllText(testXML1_MatchsXSD1, _FileEncoding);
+            string xmlNotWorking = File.ReadAllText(testXML2_MatchsXSD1Not, _FileEncoding);
+            string xsd = File.ReadAllText(testXSD1File, _FileEncoding);
+            Assert.IsTrue(GRYLibrary.Utilities.ValidateXMLAgainstXSD(xmlWorking, xsd, testTargetNamespace, out _));
+            Assert.IsFalse(GRYLibrary.Utilities.ValidateXMLAgainstXSD(xmlNotWorking, xsd, testTargetNamespace, out _));
         }
         [TestMethod]
         public void TestXSLTValidator_Test1()
         {
-            throw new System.NotImplementedException();
+            string testDataFolder = Path.Combine("TestData", "XSLTTransformator", "Test1");
+            string testSource = Path.Combine(testDataFolder, "TestTransformationSource.xml");
+            string testTarget = Path.Combine(testDataFolder, "TestTransformationTarget.xml");
+            string testXSLT = Path.Combine(testDataFolder, "TestXSLT1.xslt");
+            string xmlSource = File.ReadAllText(testSource, _FileEncoding);
+            string xmlTarget = File.ReadAllText(testTarget, _FileEncoding);
+            string xslt = File.ReadAllText(testXSLT, _FileEncoding);
+            Assert.AreEqual(xmlTarget, GRYLibrary.Utilities.ApplyXSLTToXML(xmlSource, xslt));
         }
     }
 }
