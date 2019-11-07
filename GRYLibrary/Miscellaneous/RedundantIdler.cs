96574a9c36935f4fe0a6fc03cd99b9eb70b0b76d
Marius Göcke <marius.goecke@gmail.com>
2019-11-07 15:44:42 +0100

    added RedundantIdler

---
+++ GRYLibrary/Miscellaneous/RedundantIdler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GRYLibrary.Miscellaneous
+{
+    public class RedundantIdler
+    {
+        public static void WaitUntilExecutionIsRequired()
+        {
+            if (ThisProgramIsFirstInstance())
+            {
+                StartBackupInstance();
+            }
+            else
+            {
+                WaitUntilOtherProgramFinished();
+            }
+        }
+
+        private static void WaitUntilOtherProgramFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void StartBackupInstance()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool ThisProgramIsFirstInstance()
+        {
+            return GetAmountOfProcessesWithNameOfCurrentProcess() == 1;
+        }
+
+        private static int GetAmountOfProcessesWithNameOfCurrentProcess()
+        {
+            string currentProcessName = Process.GetCurrentProcess().ProcessName;
+            return Process.GetProcessesByName(currentProcessName).Length;
+        }
+    }
+}
