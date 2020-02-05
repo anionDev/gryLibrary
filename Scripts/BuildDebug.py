85790809f3f2632d172b80232114707e7984ab22
Marius Göcke <marius.goecke@gmail.com>
2020-02-05 14:28:58 +0100

    removed scriptcollection and buildscripts

--- Scripts/BuildDebug.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-print("Start GRYLibrary Build.py Debug")
-subprocess.call("python Build.py Debug")
--- Scripts/BuildRelease.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-print("Start GRYLibrary Build.py Release")
-subprocess.call("python Build.py Release")
