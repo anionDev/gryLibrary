1b6d39350569133d25d5039009dc448619a5d0c8
Marius Göcke <marius.goecke@gmail.com>
2020-05-27 20:12:40 +0200

    added some tostring-testcases

--- GRYLibraryTests/Tests/GenericToStringTest.cs
+++ GRYLibraryTests/Tests/GenericToStringTest.cs
@@ -1,4 +1,6 @@
 ﻿using GRYLibrary.Core.AdvancedObjectAnalysis;
+using GRYLibrary.TestData.TestTypes.ComplexDataStructure;
+using GRYLibrary.TestData.TestTypes.CyclicDataStructure;
 using GRYLibrary.TestData.TestTypes.SimpleDataStructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -10,11 +12,20 @@ namespace GRYLibrary.Tests
     [TestClass]
     public class GenericToStringTest
     {
-        [Ignore]
         [TestMethod]
-        public void SimpleDataStructureToString()
+        public void SimpleDataStructureTestObjectToString()
         {
             Assert.AreEqual(@"TODO", GenericToString.Instance.ToString(SimpleDataStructure1.GetTestObject()));
+        }
+        [TestMethod]
+        public void ComplexTestObjectToString()
+        {
+            Assert.AreEqual(@"TODO", GenericToString.Instance.ToString(Company.GetRandom()));
+        }
+        [TestMethod]
+        public void CyclicTestObjectToString()
+        {
+            Assert.AreEqual(@"TODO", GenericToString.Instance.ToString(CycleA.GetRandom()));
         }
     }
 }
